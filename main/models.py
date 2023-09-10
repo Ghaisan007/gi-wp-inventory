f8577f2111f0de88dd0866284257ecae99ceb0b3
Gaelzen007 <gaelzen007@gmail.com>
2023-09-10 22:02:33 +0700

    Selesai mengerjakan tugas 2.

--- main/models.py
+++ main/models.py
@@ -5,3 +5,8 @@ class Item(models.Model):
     name = models.CharField(max_length=255)
     amount = models.IntegerField()
     description = models.TextField()
+    base_atk = models.IntegerField(null=True)
+    substat = models.CharField(max_length=255, null=True)
+    weapon_passive = models.TextField(null=True)
+    weapon_type = models.CharField(max_length=255, null=True)
+    rarity = models.CharField(max_length=255, null=True)
